b22e1fd6ea0fb3a3a792309c5d038cf285495116
dat222003 <103916721+dat222003@users.noreply.github.com>
2022-04-20 00:11:05 +0700

    add sql

--- resources/views/masters/admin_home.blade.php
+++ resources/views/masters/admin_home.blade.php
@@ -3,7 +3,7 @@
 
 @section('main')
 
-    <h1>Welcome {{ $admin_account->username }}</h1>
-
+    <h1>Welcome {{ $admin_account->full_name }}</h1>
+    <h3>You have Logged in with username: {{ $admin_account->username }} </h3>
 
 @endsection
