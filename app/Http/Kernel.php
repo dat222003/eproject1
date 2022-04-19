e249d2dd1987a734b3142dcd675478e50b07e7b4
dat222003 <103916721+dat222003@users.noreply.github.com>
2022-04-19 21:45:31 +0700

    add middleware check for logged in

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -53,6 +53,7 @@ class Kernel extends HttpKernel
      * @var array<string, class-string|string>
      */
     protected $routeMiddleware = [
+        'notloggedin' => \App\Http\Middleware\Auth_admin::class,
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'auth.session' => \Illuminate\Session\Middleware\AuthenticateSession::class,
--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ Route::group(['prefix' => 'eproject'], function (){
         'uses' => 'App\Http\Controllers\eprojectController@admin',
         'as' => 'admin.home'
 
-    ]);
+    ])->middleware('notloggedin');
 
 //admin
     Route::get('admin', [
@@ -43,7 +43,7 @@ Route::group(['prefix' => 'eproject'], function (){
         'uses' => 'App\Http\Controllers\eprojectController@logout',
         'as' => 'admin.logout'
 
-    ]);
+    ])->middleware('notloggedin');
 
 
 
@@ -52,7 +52,7 @@ Route::group(['prefix' => 'eproject'], function (){
         'uses' => 'App\Http\Controllers\eprojectController@index_category',
         'as' => 'admin.index.category'
 
-    ]);
+    ])->middleware('notloggedin');
     Route::get('/phpinfo', function() {
         return phpinfo();
     });
@@ -61,42 +61,42 @@ Route::group(['prefix' => 'eproject'], function (){
         'uses' => 'App\Http\Controllers\eprojectController@show_category',
         'as' => 'admin.detail.category'
 
-    ]);
+    ])->middleware('notloggedin');
 
 
     Route::get('delete/{id}', [
         'uses' => 'App\Http\Controllers\eprojectController@confirm_category',
         'as' => 'admin.delete.confirm.category'
 
-    ]);
+    ])->middleware('notloggedin');
     Route::post('delete/{id}', [
         'uses' => 'App\Http\Controllers\eprojectController@delete_category',
         'as' => 'admin.delete.category'
 
-    ]);
+    ])->middleware('notloggedin');
 
     Route::get('update/{id}',[
         'uses' => 'App\Http\Controllers\eprojectController@edit_category',
         'as' => 'admin.edit.category'
-    ]);
+    ])->middleware('notloggedin');
 
     Route::post('update/{id}',[
         'uses' => 'App\Http\Controllers\eprojectController@update_category',
         'as' => 'admin.update.category'
-    ]);
+    ])->middleware('notloggedin');
 
 
     Route::get('new_category_form', [
         'uses' => 'App\Http\Controllers\eprojectController@form_category',
         'as' => 'admin.create.category'
 
-    ]);
+    ])->middleware('notloggedin');
 
     Route::post('new_category_form', [
         'uses' => 'App\Http\Controllers\eprojectController@store_category',
         'as' => 'admin.store.category'
 
-    ]);
+    ])->middleware('notloggedin');
 
 
     //product
@@ -105,48 +105,48 @@ Route::group(['prefix' => 'eproject'], function (){
         'uses' => 'App\Http\Controllers\eprojectController@index_product',
         'as' => 'admin.index.product'
 
-    ]);
+    ])->middleware('notloggedin');
 
     Route::get('detail_product/{id}', [
         'uses' => 'App\Http\Controllers\eprojectController@show_product',
         'as' => 'admin.detail.product'
 
-    ]);
+    ])->middleware('notloggedin');
 
 
     Route::get('delete_product/{id}', [
         'uses' => 'App\Http\Controllers\eprojectController@confirm_product',
         'as' => 'admin.delete.confirm.product'
 
-    ]);
+    ])->middleware('notloggedin');
     Route::post('delete_product/{id}', [
         'uses' => 'App\Http\Controllers\eprojectController@delete_product',
         'as' => 'admin.delete.product'
 
-    ]);
+    ])->middleware('notloggedin');
 
     Route::get('update_product/{id}/{categoryid}',[
         'uses' => 'App\Http\Controllers\eprojectController@edit_product',
         'as' => 'admin.edit.product'
-    ]);
+    ])->middleware('notloggedin');
 
     Route::post('update_product/{id}',[
         'uses' => 'App\Http\Controllers\eprojectController@update_product',
         'as' => 'admin.update.product'
-    ]);
+    ])->middleware('notloggedin');
 
 
     Route::get('new_product_form', [
         'uses' => 'App\Http\Controllers\eprojectController@form_product',
         'as' => 'admin.create.product'
 
-    ]);
+    ])->middleware('notloggedin');
 
     Route::post('new_product_form', [
         'uses' => 'App\Http\Controllers\eprojectController@store_product',
         'as' => 'admin.store.product'
 
-    ]);
+    ])->middleware('notloggedin');
 
 
 //    service
@@ -160,47 +160,47 @@ Route::group(['prefix' => 'eproject'], function (){
         'uses' => 'App\Http\Controllers\eprojectController@index_service',
         'as' => 'admin.index.service'
 
-    ]);
+    ])->middleware('notloggedin');
 
     Route::get('detail_service/{id}', [
         'uses' => 'App\Http\Controllers\eprojectController@show_service',
         'as' => 'admin.detail.service'
 
-    ]);
+    ])->middleware('notloggedin');
 
 
     Route::get('delete_service/{id}', [
         'uses' => 'App\Http\Controllers\eprojectController@confirm_service',
         'as' => 'admin.delete.confirm.service'
 
-    ]);
+    ])->middleware('notloggedin');
     Route::post('delete_service/{id}', [
         'uses' => 'App\Http\Controllers\eprojectController@delete_service',
         'as' => 'admin.delete.service'
 
-    ]);
+    ])->middleware('notloggedin');
 
     Route::get('update_service/{id}/{categoryid}',[
         'uses' => 'App\Http\Controllers\eprojectController@edit_service',
         'as' => 'admin.edit.service'
-    ]);
+    ])->middleware('notloggedin');
 
     Route::post('update_service/{id}',[
         'uses' => 'App\Http\Controllers\eprojectController@update_service',
         'as' => 'admin.update.service'
-    ]);
+    ])->middleware('notloggedin');
 
 
     Route::get('new_service_form', [
         'uses' => 'App\Http\Controllers\eprojectController@form_service',
         'as' => 'admin.create.service'
 
-    ]);
+    ])->middleware('notloggedin');
 
     Route::post('new_service_form', [
         'uses' => 'App\Http\Controllers\eprojectController@store_service',
         'as' => 'admin.store.service'
 
-    ]);
+    ])->middleware('notloggedin');
 
 });
