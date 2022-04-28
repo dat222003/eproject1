9b2a029072e76d64a3bc5b6afc843cc824095c81
TrungHiuu <103981037+TrungHiuu@users.noreply.github.com>
2022-04-28 23:06:20 +0700

    PATIENT

--- resources/views/masters/home/patient_education.blade.php
+++ resources/views/masters/home/patient_education.blade.php
@@ -2,15 +2,129 @@
 
 
 @section('main')
+    <style>
+        .container{
+            padding: 40px 50px 50px 50px;
+
+        }
+
+        .step{
+            align-content: center;
+            border-left: solid 3px #0d6efd;
+            padding-left: 10px;
+            margin-bottom: 30px;
+            margin-top: 30px;
+        }
+
+        body{
+            line-height: 30px;
+            color: black;
+        }
 
 
-    {{--    <div>--}}
-    {{--        <img class="top_background" src="{{ url('img/system/dark.png') }}" alt="image">--}}
-    {{--    </div>--}}
-    {{--    <section>--}}
-    {{--        Content goes here--}}
+    </style>
 
-    {{--    </section>--}}
+    <div >
+        <img class="top_background" src="{{ url('img/system/dark.png') }}" alt="image">
+    </div>
 
+    <section class="container">
+
+        <h2 class="text-center">Educating Patients on the Importance of Oral Health and Routine Dental Care</h2>
+        <p> Making sure patients are well informed of the importance of oral health before they even book their hygiene appointment can help curb anxiety or other barriers that may be preventing them from coming into the dental office. And with staff training on how to communicate key oral health messages with patients, the oral hygiene appointment can be the perfect way to build relationships and trust with patients.
+        </p>
+
+        <br>
+        <p>As any dental professional knows, oral hygiene plays a critical role in our overall health and wellbeing, but when it comes to educating our patients, knowing what information to give out can be the breakdown of a huge barrier for preventative care. Making sure patients are well informed of the importance of oral health before they even book their hygiene appointment can help curb anxiety or other barriers that may be preventing them from coming into the dental office. And with staff training on how to communicate key oral health messages with patients, the oral hygiene appointment can be the perfect way to build relationships and trust with patients to assure they continue making their six-month checkups.</p>
+        <div class="step">
+            <div class="text-primary text-sm"></div>
+            <h5><strong>Educate through your marketing campaigns</strong></h5>
+        </div>
+        <div>
+            A huge obstacle for many patients is scheduling preventative care appointments due to fear and anxiety; insurance and payment worries; and an overall lack of education on the importance these visits have on overall health.
+
+            To help patients overcome these barriers, reach out to your patients on your marketing platforms. Use social media, email and direct mail to educate and bring awareness to oral health. Keep in mind awareness months for oral hygiene and really celebrate those months with your patients; the more information you share with them, the more they will start to understand and prioritize their own health.
+
+            Sharing oral health tips from the Centers for Disease Control and Prevention (CDC) and expanding on those tips is an easy way to keep a steady stream of relevant content on your social media channels; these tips can also be incorporated in your print or email campaigns. Be sure to stay consistent with your communication; the more patients hear from you the more comfortable they will feel scheduling and keeping their recall appointments.
+
+        </div>
+        <div class="step">
+            <div class="text-primary text-sm"></div>
+            <h5><strong>Educate your staff</strong></h5>
+        </div>
+        <div>
+            Your staff no doubt understand the importance of oral hygiene, but do they fully understand how to communicate that importance to your patients? Creating educational materials and holding training sessions on how to successfully communicate to patients through your staff will not only help your practice stay aligned with its goals, but it will also help your patients see that you are a united front and will take what you say seriously.
+
+            Creating practice scripts for your staff to talk to your patients can be a great starting point. Have your staff practice how to effectively communicate the importance of oral hygiene. Walking through pain points that your patients might have will also help ease fears and anxiety.
+        </div>
+        <div class="step">
+            <div class="text-primary text-sm"></div>
+            <h5><strong>Educate during the oral hygiene appointment</strong></h5>
+        </div>
+
+        <div>
+            Most people know the importance of brushing and flossing daily, but do your patients truly understand the full benefits? Educating your patients can be as easy as taking time at the end of their exam to explain why these checkups are so valuable. Explain that these maintenance and preventative checkups are helping them avoid potential future issues with not only their oral health but their overall health and wellbeing. From early caries detection to early gum disease, there are numerous oral issues that can be caught early enough to reverse or treat before they turn into a bigger problem. Educating your patients on those possibilities will help them value their time with you. Using a visual aid such as this infographic from Delta Dental can help steer the conversation while also helping the patient better understand the connection of oral health with overall health.
+
+            As you’re explaining the importance of good oral hygiene, be sensitive to the patient by listening to their concerns or reactions to your advice; encourage them to ask questions. After the cleaning and visit with the dentist, be sure to ask if they have any additional questions before scheduling their next appointment.
+        </div>
+        <div class="step">
+            <div class="text-primary text-sm"></div>
+            <h5><strong>Plan, practice, execute</strong></h5>
+        </div>
+        <div>
+            With the ongoing COVID-19 pandemic, patient anxiety may be elevated more than normal, and the importance of preventative care may be pushed to the side. Practice these communication ideas with your staff on how to relay to the patient all the precautions your office is taking during this time. Many practice owners were caught off guard when the World Health Organization (WHO) recently recommended limiting routine dental care, but as Roger P. Levin, DDS, founder and CEO of Levin Group, explained in a video response, having a plan that includes a script and staff training can help keep your patients at ease and your schedule full.
+
+            Educating patients on the importance of oral hygiene and helping them understand how it can affect overall health can play a role in patients seeing the value of preventative appointments. Beyond using time in the chair to educate patients, practice owners should also use traditional and digital marketing communications to educate patients on the importance of maintaining good oral health. Coaching your staff on how to handle sensitive conversations with your patients will help create a consistent culture of education throughout your practice.
+
+        </div>
+{{--        <div class="step">--}}
+{{--            <div class="text-primary text-sm">Step 5</div>--}}
+{{--            <h5>Undertake other forms of certification or licensure</h5>--}}
+{{--        </div>--}}
+{{--        <div>--}}
+{{--            In addition to the CDA examination, students may also choose to complete the National Entry Level Dental Assistant test, which the DANB also administers. Students looking to work in specialized dental assisting can undertake further certifications, including:--}}
+
+{{--            <ul>--}}
+{{--                <li>Orthodontic Dental Assistant</li>--}}
+{{--                <li>Preventive Functions Dental Assistant</li>--}}
+{{--                <li>Restorative Functions Dental Assistant</li>--}}
+{{--            </ul>--}}
+{{--        </div>--}}
+{{--        <div class="step">--}}
+{{--            <div class="text-primary text-sm">Step 6</div>--}}
+{{--            <h5>Land an entry-level job</h5>--}}
+{{--        </div>--}}
+{{--        <div>--}}
+{{--            Once you graduate, you should have a variety of job opportunities, either in a general dentistry setting or in a specialty area. Dental assisting is one of the fastest growing occupations in the U.S., and the Bureau of Labor Statistics reports that the number of dental assistant positions is set to grow 7% nationally between 2019 and 2029, much higher than the overall average for job growth.--}}
+{{--        </div>--}}
+{{--        <div class="step">--}}
+{{--            <div class="text-primary text-sm">Step 7</div>--}}
+{{--            <h5>Return to school</h5>--}}
+{{--        </div>--}}
+{{--        <div>--}}
+{{--            After working for a number of years, dental assistants may choose to complete an advanced degree. Those who obtained an associate degree during their initial academic training can often transfer existing credits to a bachelor’s degree in dental hygiene. This 4-year degree opens the door for increased career mobility and greater earning potential.--}}
+{{--        </div>--}}
+{{--        <h2 class="text-center">What Does a Dental Assistant Do?</h2>--}}
+{{--        <div id="dig-deeper" class="c-dig-deeper flex flex-col mb-9 md:mb-12">--}}
+{{--            <div class="bg-quaternary-50 p-8 rounded-t-lg">--}}
+{{--                <div class="text-lg font-bold text-gray-600 mb-2">Dental Assistant Career Basics</div>--}}
+{{--                <p class="text-base">Dental assistants are frequently seen as crucial members of the dental team, providing a spectrum of services to support both dentists and administrative staff. While some states do not require formal licensure, others do, meaning that paths to the workforce vary. It also means the work dental assistants do may vary.</p>--}}
+{{--            </div>--}}
+{{--            <div id="deeper" class="bg-primary">--}}
+{{--                <div class="collapse" id="collapseExample">--}}
+{{--                    <div class="card card-body">--}}
+{{--                        <p>--}}
+{{--                            A dental assisting program can take anywhere from nine months to two years to complete, depending on whether it’s a certificate, diploma or degree program. After finishing their education and passing any required certifications, dental assistants perform a variety of essential functions. Administrative tasks may include scheduling and confirming appointments, maintaining patient records and ordering dental supplies. Clinical duties include handing tools to the dentist during treatment and examination, sterilizing instruments, operating suction devices, processing X-rays and making impressions for dental fixtures.--}}
+{{--                        </p>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--                <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">--}}
+{{--                    Button with data-bs-target--}}
+{{--                </button>--}}
+{{--            </div>--}}
+
+
+
+    </section>
 
 @endsection
