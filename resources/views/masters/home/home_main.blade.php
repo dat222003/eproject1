5f5e557bf5ca67d22b4d79ee96696c32e858b3d1
dat222003 <103916721+dat222003@users.noreply.github.com>
2022-05-03 20:11:00 +0700

    disabled service on home page

--- resources/views/masters/home/home_main.blade.php
+++ resources/views/masters/home/home_main.blade.php
@@ -99,67 +99,67 @@
 
 
     <!-- Service Start -->
-    <div class="container-fluid py-5 wow fadeInUp" data-wow-delay="0.1s">
-        <div class="container py-5">
-            <div class="section-title text-center position-relative pb-3 mb-5 mx-auto" style="max-width: 600px;">
-                <h5 class="fw-bold text-primary text-uppercase">Our Services</h5>
-                {{--            <h1 class="mb-0">Custom IT Solutions for Your Successful Business</h1>--}}
-            </div>
-            <div class="row g-5">
-                <div class="col-lg-4 col-md-6 wow zoomIn" data-wow-delay="0.3s">
-                    <div class="service-item bg-light rounded d-flex flex-column align-items-center justify-content-center text-center">
-                        <h4 class="mb-3">Veneers</h4>
-                        <img src="{{ url('img/system/veneers-1.png') }}" alt="">
-                        <a class="btn btn-lg btn-primary rounded" href="#">
-                            <i class="bi bi-arrow-right"></i>
-                        </a>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6 wow zoomIn" data-wow-delay="0.6s">
-                    <div class="service-item bg-light rounded d-flex flex-column align-items-center justify-content-center text-center">
-                        <h4 class="mb-3">Teeth Whitening</h4>
-                        <img src="{{ url('img/system/teeth-whitenings-1.png') }}" alt="image">
-                        <a class="btn btn-lg btn-primary rounded" href="#">
-                            <i class="bi bi-arrow-right"></i>
-                        </a>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6 wow zoomIn" data-wow-delay="0.9s">
-                    <div class="service-item bg-light rounded d-flex flex-column align-items-center justify-content-center text-center">
-                        <h4 class="mb-3">Dental Bridges</h4>
-                        <img src="{{ url('img/system/dental-bridges-1.png') }}" alt="image">
-                        <a class="btn btn-lg btn-primary rounded" href="#">
-                            <i class="bi bi-arrow-right"></i>
-                        </a>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6 wow zoomIn" data-wow-delay="0.3s">
-                    <div class="service-item bg-light rounded d-flex flex-column align-items-center justify-content-center text-center">
-                        <h4 class="mb-3">Dental Crowns</h4>
-                        <img src="{{ url('img/system/dental-crown-1.png') }}" alt="image">
-                        <a class="btn btn-lg btn-primary rounded" href="#">
-                            <i class="bi bi-arrow-right"></i>
-                        </a>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6 wow zoomIn" data-wow-delay="0.6s">
-                    <div class="service-item bg-light rounded d-flex flex-column align-items-center justify-content-center text-center">
-                        <h4 class="mb-3">Root Canal</h4>
-                        <img src="{{ url('img/system/root-canal-1.png') }}" alt="image">
-                        <a class="btn btn-lg btn-primary rounded" href="#">
-                            <i class="bi bi-arrow-right"></i>
-                        </a>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6 wow zoomIn" data-wow-delay="0.9s">
-                    <div class="position-relative bg-primary rounded h-100 d-flex flex-column align-items-center justify-content-center text-center p-5">
-                        <p class="text-white mb-3">Call Us For More Details</p>
-                        <h2 class="text-white mb-0">+012 345 6789</h2>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+{{--    <div class="container-fluid py-5 wow fadeInUp" data-wow-delay="0.1s">--}}
+{{--        <div class="container py-5">--}}
+{{--            <div class="section-title text-center position-relative pb-3 mb-5 mx-auto" style="max-width: 600px;">--}}
+{{--                <h5 class="fw-bold text-primary text-uppercase">Our Services</h5>--}}
+{{--                --}}{{--            <h1 class="mb-0">Custom IT Solutions for Your Successful Business</h1>--}}
+{{--            </div>--}}
+{{--            <div class="row g-5">--}}
+{{--                <div class="col-lg-4 col-md-6 wow zoomIn" data-wow-delay="0.3s">--}}
+{{--                    <div class="service-item bg-light rounded d-flex flex-column align-items-center justify-content-center text-center">--}}
+{{--                        <h4 class="mb-3">Veneers</h4>--}}
+{{--                        <img src="{{ url('img/system/veneers-1.png') }}" alt="">--}}
+{{--                        <a class="btn btn-lg btn-primary rounded" href="#">--}}
+{{--                            <i class="bi bi-arrow-right"></i>--}}
+{{--                        </a>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--                <div class="col-lg-4 col-md-6 wow zoomIn" data-wow-delay="0.6s">--}}
+{{--                    <div class="service-item bg-light rounded d-flex flex-column align-items-center justify-content-center text-center">--}}
+{{--                        <h4 class="mb-3">Teeth Whitening</h4>--}}
+{{--                        <img src="{{ url('img/system/teeth-whitenings-1.png') }}" alt="image">--}}
+{{--                        <a class="btn btn-lg btn-primary rounded" href="#">--}}
+{{--                            <i class="bi bi-arrow-right"></i>--}}
+{{--                        </a>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--                <div class="col-lg-4 col-md-6 wow zoomIn" data-wow-delay="0.9s">--}}
+{{--                    <div class="service-item bg-light rounded d-flex flex-column align-items-center justify-content-center text-center">--}}
+{{--                        <h4 class="mb-3">Dental Bridges</h4>--}}
+{{--                        <img src="{{ url('img/system/dental-bridges-1.png') }}" alt="image">--}}
+{{--                        <a class="btn btn-lg btn-primary rounded" href="#">--}}
+{{--                            <i class="bi bi-arrow-right"></i>--}}
+{{--                        </a>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--                <div class="col-lg-4 col-md-6 wow zoomIn" data-wow-delay="0.3s">--}}
+{{--                    <div class="service-item bg-light rounded d-flex flex-column align-items-center justify-content-center text-center">--}}
+{{--                        <h4 class="mb-3">Dental Crowns</h4>--}}
+{{--                        <img src="{{ url('img/system/dental-crown-1.png') }}" alt="image">--}}
+{{--                        <a class="btn btn-lg btn-primary rounded" href="#">--}}
+{{--                            <i class="bi bi-arrow-right"></i>--}}
+{{--                        </a>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--                <div class="col-lg-4 col-md-6 wow zoomIn" data-wow-delay="0.6s">--}}
+{{--                    <div class="service-item bg-light rounded d-flex flex-column align-items-center justify-content-center text-center">--}}
+{{--                        <h4 class="mb-3">Root Canal</h4>--}}
+{{--                        <img src="{{ url('img/system/root-canal-1.png') }}" alt="image">--}}
+{{--                        <a class="btn btn-lg btn-primary rounded" href="#">--}}
+{{--                            <i class="bi bi-arrow-right"></i>--}}
+{{--                        </a>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--                <div class="col-lg-4 col-md-6 wow zoomIn" data-wow-delay="0.9s">--}}
+{{--                    <div class="position-relative bg-primary rounded h-100 d-flex flex-column align-items-center justify-content-center text-center p-5">--}}
+{{--                        <p class="text-white mb-3">Call Us For More Details</p>--}}
+{{--                        <h2 class="text-white mb-0">+012 345 6789</h2>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--    </div>--}}
     <!-- Service End -->
 
 
