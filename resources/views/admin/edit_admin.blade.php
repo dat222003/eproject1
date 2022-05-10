cbbb712649792dc609bb816a6227be9400064f3a
dat222003 <103916721+dat222003@users.noreply.github.com>
2022-05-10 20:21:40 +0700

    return old value

--- resources/views/admin/edit_admin.blade.php
+++ resources/views/admin/edit_admin.blade.php
@@ -13,32 +13,32 @@
             <div class="form-group col-md-6">
                 <label for="username" class="font-weight-bold">User Name</label>
                 <input type="text" class="form-control" id="username" name="username"
-                       value="{{old('username')?? $admin_account->username}}">
+                       value="{{old('username')?? $request->username ?? $admin_account->username}}">
 
             </div>
 
             <div class="form-group col-md-6">
                 <label for="full_name" class="font-weight-bold">Full Name</label>
                 <input type="text" class="form-control" id="full_name" name="full_name"
-                       value="{{old('full_name')?? $admin_account->full_name}}">
+                       value="{{old('full_name')?? $request->full_name ?? $admin_account->full_name}}">
             </div>
 
             <div class="form-group col-md-6">
                 <label for="email" class="font-weight-bold">Email</label>
                 <input type="email" class="form-control" id="email" name="email"
-                       value="{{old('email')?? $admin_account->email}}">
+                       value="{{old('email')?? $request->email ?? $admin_account->email}}">
             </div>
 
             <div class="form-group col-md-6">
                 <label for="phone" class="font-weight-bold">Phone</label>
                 <input type="tel" class="form-control" id="phone" name="phone"
-                       value="{{old('phone')?? $admin_account->phone}}">
+                       value="{{old('phone')?? $request->phone ?? $admin_account->phone}}">
             </div>
 
             <div class="form-group col-md-6">
                 <label for="password" class="font-weight-bold">Password</label>
                 <input type="password" class="form-control" id="password" name="password"
-                       value="{{old('password')}}"
+                       value="{{old('password'?? $request->password )}}"
                        placeholder="Please Confirm Your Password">
             </div>
 
